f723bda70dade18dc75492cd588a2446f3c32064
geoffrey woodcock <theatomproject@gmail.com>
2018-04-25 17:46:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,24 +42,5 @@ class Song
   end
 
   #artist_count returns hash of artists and their songs
-  
-#   def add_student(name, grade)
-#     if @roster.has_key?(grade)
-#       @roster[grade] << name
-#     else
-#       @roster[grade] = [name]
-#     end
-#   end
-  
-#   def grade(grade)
-#     self.roster[grade]
-#   end
-  
-#   def sort 
-#   self.roster.each do |grade, students|
-#     students.sort!
-#   end
-# end
-
 
 end
