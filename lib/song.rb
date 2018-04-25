e014b6a89dc5be7df8b1f0acd05ffbc4bd725615
geoffrey woodcock <theatomproject@gmail.com>
2018-04-25 16:44:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,12 @@
 class Song 
   
-   @@count = 0
+  @@count = 0
   
-   def initialize
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1
   end
 
